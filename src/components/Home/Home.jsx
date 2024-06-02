a873d2999487998a75fb6c9cea1bcb4fa445658c
thesumanth <bojugusumanthkumar@gmail.com>
2024-06-02 22:41:10 +0530

    fixed bug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import image from "../../assets/home-image.png";
 import { Link } from "react-router-dom";
-import onTime from "../../assets/ontime.gif";
+import onTime from "../../assets/onTime.gif";
 import professionalTeam from "../../assets/professionalTeam.gif";
 import analyze from "../../assets/analyze.gif";
 import minimize from "../../assets/minimize.gif";
